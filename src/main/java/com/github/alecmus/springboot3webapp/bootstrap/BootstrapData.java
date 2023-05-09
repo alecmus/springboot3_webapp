bfc765661903ee31e8e6b0dd47b300bdcf8e834a
Alec Musasa <alecmus@live.com>
2023-05-09 16:03:56 +0200

    Added Publisher JPA relationships

--- src/main/java/com/github/alecmus/springboot3webapp/bootstrap/BootstrapData.java
+++ src/main/java/com/github/alecmus/springboot3webapp/bootstrap/BootstrapData.java
@@ -49,18 +49,22 @@ public class BootstrapData implements CommandLineRunner {
         ericSaved.getBooks().add(dddSaved);
         rodSaved.getBooks().add(noEJBSaved);
 
+        Publisher publisher = new Publisher();
+        publisher.setPublisherName("My Publisher");
+        publisher.setAddress("123 Main");
+        Publisher savedPublisher = publisherRepository.save(publisher);
+
+        dddSaved.setPublisher(savedPublisher);
+        noEJBSaved.setPublisher(savedPublisher);
+
         authorRepository.save(ericSaved);
         authorRepository.save(rodSaved);
+        bookRepository.save(dddSaved);
+        bookRepository.save(noEJBSaved);
 
         System.out.println("In Bootstrap");
         System.out.println("Author count: " + authorRepository.count());
         System.out.println("Book count: " + bookRepository.count());
-
-        Publisher publisher = new Publisher();
-        publisher.setPublisherName("My Publisher");
-        publisher.setAddress("123 Main");
-        publisherRepository.save(publisher);
-
         System.out.println("Publisher count: " + publisherRepository.count());
     }
 }
--- src/main/java/com/github/alecmus/springboot3webapp/domain/Publisher.java
+++ src/main/java/com/github/alecmus/springboot3webapp/domain/Publisher.java
@@ -1,9 +1,8 @@
 package com.github.alecmus.springboot3webapp.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.Set;
 
 @Entity
 public class Publisher {
@@ -15,6 +14,9 @@ public class Publisher {
     private String city;
     private String state;
     private String zipCode;
+
+    @OneToMany(mappedBy = "publisher")
+    private Set<Book> books;
 
     public Long getId() {
         return id;
