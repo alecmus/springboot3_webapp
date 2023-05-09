0dc5c963179db49b7b996212b1c13576bd75af14
Alec Musasa <alecmus@live.com>
2023-05-09 17:01:32 +0200

    Fixed Author-Book bootstrapping

--- src/main/java/com/github/alecmus/springboot3webapp/bootstrap/BootstrapData.java
+++ src/main/java/com/github/alecmus/springboot3webapp/bootstrap/BootstrapData.java
@@ -48,6 +48,8 @@ public class BootstrapData implements CommandLineRunner {
 
         ericSaved.getBooks().add(dddSaved);
         rodSaved.getBooks().add(noEJBSaved);
+        dddSaved.getAuthors().add(ericSaved);
+        noEJBSaved.getAuthors().add(rodSaved);
 
         Publisher publisher = new Publisher();
         publisher.setPublisherName("My Publisher");
