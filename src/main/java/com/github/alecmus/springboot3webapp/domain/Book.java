1b1651e7404572889fa4c4dcdbaf205e18bbfe08
Alec Musasa <alecmus@live.com>
2023-05-09 14:39:31 +0200

    Added relationships to Book and Author Entities

--- src/main/java/com/github/alecmus/springboot3webapp/domain/Book.java
+++ src/main/java/com/github/alecmus/springboot3webapp/domain/Book.java
@@ -1,9 +1,8 @@
 package com.github.alecmus.springboot3webapp.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.Set;
 
 @Entity
 public class Book {
@@ -12,6 +11,19 @@ public class Book {
     private Long id;
     private String title;
     private String isbn;
+
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<Author> authors;
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
 
     public Long getId() {
         return id;
