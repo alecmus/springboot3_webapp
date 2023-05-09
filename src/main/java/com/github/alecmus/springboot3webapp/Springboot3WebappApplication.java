00b53004df7aac31753e9c13e44215d988d7d88d
Alec Musasa <alecmus@live.com>
2023-05-09 14:37:20 +0200

    Added Spring Initializr generated code

---
+++ src/main/java/com/github/alecmus/springboot3webapp/Springboot3WebappApplication.java
@@ -0,0 +1,13 @@
+package com.github.alecmus.springboot3webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3WebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3WebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/alecmus/springboot3webapp/Springboot3WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.alecmus.springboot3webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
