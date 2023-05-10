d1144c910e77ad22f3b68059d222e1f32f883af2
Alec Musasa <alecmus@live.com>
2023-05-10 03:54:31 +0200

    Added Controller: BookController

---
+++ src/main/java/com/github/alecmus/springboot3webapp/controllers/BookController.java
@@ -0,0 +1,21 @@
+package com.github.alecmus.springboot3webapp.controllers;
+
+import com.github.alecmus.springboot3webapp.services.BookService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+        model.addAttribute("books", bookService.findAll());
+        return "books";
+    }
+}
